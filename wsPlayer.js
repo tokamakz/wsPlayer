31aeb44aa4899d9f2ab19d1c4b4d1a1cbef10418
zhouweimin <zhouweimin@supremin.com>
2021-11-13 08:10:30 +0000

    bugfix:增加自动清理已播放的sourcebuffer

--- wsPlayer.js
+++ wsPlayer.js
@@ -18,21 +18,50 @@ wsPlayer.prototype.open = function () {
         for (let i = 0; i < info.tracks.length; i++) {
             codecs.push(info.tracks[i].codec);
         }
+        console.log(codecs);
         let video = document.getElementById(this.videoId);
         let mediasource = new MediaSource();
         video.src = URL.createObjectURL(mediasource);
-    
+        
+        let pre_pos = 0;
         mediasource.onsourceopen = function() {
             sourcebuffer = mediasource.addSourceBuffer('video/mp4; codecs="' + codecs.join(', ') + '"');
             sourcebuffer.onupdateend = function() {
+                let pos = video.currentTime;
+                //console.log("video.buffered.length=" + video.buffered.length);
                 if(video.buffered.length > 0) {
-                    let pos = video.currentTime;
-                    let start = video.buffered.start(0);
-                    //console.log("pos=" + pos + ",start=" + start);
+                    let start = video.buffered.start(video.buffered.length - 1);
+                    let end = video.buffered.end(video.buffered.length - 1);
+                    //console.log("pos=" + pos + ",start=" + start + ",end=" + end);
                     if (pos < start) {
+                        //console.log("set video.currentTime pos=" + pos + ",start=" + start + ",end=" + end);
                         video.currentTime = start;
                     }
+        
+                    if (pos - pre_pos != 0 && end - pos > 3) {
+                        //console.log("set end video.currentTime pos=" + pos + ",start=" + start + ",end=" + end);
+                        video.currentTime = end;
+                    }
+        
+                    for (let i = 0; i < video.buffered.length - 1; i++) {
+                        let prestart = video.buffered.start(i);
+                        let preend = video.buffered.end(i);
+                        if(!sourcebuffer.updating) {
+                            sourcebuffer.remove(prestart, preend);
+                        }
+                    }
+
+                    if(pos - start > 10 && !sourcebuffer.updating) {
+                        //console.warn("remove start pos=" + pos + ",start=" + start + ",end=" + end);
+                        sourcebuffer.remove(0, pos - 3);
+                    }
+        
+                    if(end - pos > 10 && !sourcebuffer.updating) {
+                        //console.warn("remove end pos=" + pos + ",start=" + start + ",end=" + end);
+                        sourcebuffer.remove(0, end - 3);
+                    }
                 }
+                pre_pos = pos;
             }
         }
     }.bind(this);
