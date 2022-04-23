f4eed07d31bcef17fc5e0c0673723774781becee
zhouweimin <zhouweimin@supremind.com>
2022-04-23 16:20:56 +0800

    close #6 fixbug:若frameQueue有积压，则一次性appendBuffer;

--- wsPlayer.js
+++ wsPlayer.js
@@ -5,6 +5,7 @@ function wsPlayer(videoId, wsUrl) {
     this.wsUrl = wsUrl;
     this.ws = null;
     this.frameQueue = [];
+    console.log("wsPlayer v1.0.1 20220423");
 }
 
 wsPlayer.prototype.open = function () {
@@ -22,37 +23,31 @@ wsPlayer.prototype.open = function () {
         let video = document.getElementById(this.videoId);
         let mediasource = new MediaSource();
         video.src = URL.createObjectURL(mediasource);
-        
         let pre_pos = 0;
         mediasource.onsourceopen = function() {
             sourcebuffer = mediasource.addSourceBuffer('video/mp4; codecs="' + codecs.join(', ') + '"');
             sourcebuffer.onupdateend = function() {
                 let pos = video.currentTime;
-                //console.log("video.buffered.length=" + video.buffered.length);
                 if(video.buffered.length > 0) {
                     let start = video.buffered.start(video.buffered.length - 1);
                     let end = video.buffered.end(video.buffered.length - 1);
                     //console.log("pos=" + pos + ",start=" + start + ",end=" + end);
 
-                    //当前播放时刻小于于buffered的开始时间
                     if (pos < start) {
                         //console.log("set video.currentTime pos=" + pos + ",start=" + start + ",end=" + end);
                         video.currentTime = start;
                     }
 
-                    //当前播放时刻大于buffered的结束时间
                     if(pos > end) {
                         //console.warn("chase frame pos=" + pos + ",start=" + start + ",end=" + end);
                         video.currentTime = start;
                     }
 
-                    //浏览器重新聚集时，追帧
                     if (pos - pre_pos != 0 && end - pos > 3) {
                         //console.log("set end video.currentTime pos=" + pos + ",start=" + start + ",end=" + end);
                         video.currentTime = end;
                     }
 
-                    //删除前面的片段，永远只保留最后一个片段
                     for (let i = 0; i < video.buffered.length - 1; i++) {
                         let prestart = video.buffered.start(i);
                         let preend = video.buffered.end(i);
@@ -61,13 +56,11 @@ wsPlayer.prototype.open = function () {
                         }
                     }
 
-                    //清理已经播放的视频片段
                     if(pos - start > 10 && !sourcebuffer.updating) {
                         //console.warn("remove start pos=" + pos + ",start=" + start + ",end=" + end);
                         sourcebuffer.remove(0, pos - 3);
                     }
 
-                    //浏览器页面未聚焦时，清理收到的视频
                     if(end - pos > 10 && !sourcebuffer.updating) {
                         //console.warn("remove end pos=" + pos + ",start=" + start + ",end=" + end);
                         sourcebuffer.remove(0, end - 3);
@@ -91,7 +84,23 @@ wsPlayer.prototype.open = function () {
         if (!sourcebuffer || sourcebuffer.updating) {
             return;
         }
-        sourcebuffer.appendBuffer(this.frameQueue.shift());
+        if(this.frameQueue.length === 1) {
+            sourcebuffer.appendBuffer(this.frameQueue.shift());
+        } else {
+            let byte_length = 0;
+            for (const qnode of this.frameQueue) {
+                byte_length += qnode.byteLength;
+            }
+            let mp4buf = new Uint8Array(byte_length);
+            let offset = 0;
+            for (const qnode of this.frameQueue) {
+                let frame = new Uint8Array(qnode);
+                mp4buf.set(frame, offset);
+                offset += qnode.byteLength;
+            }
+            sourcebuffer.appendBuffer(mp4buf);
+            this.frameQueue.splice(0, this.frameQueue.length);
+        }
     }.bind(this);
 }
 
