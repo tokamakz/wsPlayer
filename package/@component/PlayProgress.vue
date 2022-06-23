e51dae227f3cb5635d30995561d716e438d2697e
lanfengcloud <localfeng@163.com>
2022-06-24 00:25:02 +0800

    feat: 添加进度条样式，拆分播放按钮和进度条组件

---
+++ package/@component/PlayProgress.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="wsp-progress-bar" tabindex='-1' role='slider' aria-label='播放滑块' aria-valuemin='0' aria-valuemax='57'
+    aria-valuenow='4' aria-valuetext='0 分钟 7 秒/0 分钟 57 秒'>
+    <div class="wsp-chapters-container" style='height: 7px;'>
+      <div class="wsp-chapter-hover-container" style="width: 100%;">
+        <div class="wsp-progress-bar-padding"></div>
+        <div class="wsp-progress-list">
+          <div class="wsp-play-progress wsp-swatch-background-color" style="left: 0px; transform: scaleX(0.137041);">
+          </div>
+          <div class="wsp-progress-linear-live-buffer"></div>
+          <div class="wsp-load-progress" style="left: 0px; transform: scaleX(1);"></div>
+          <div class="wsp-hover-progress" style="left: 84.417px; transform: scaleX(0);"></div>
+          <div class="wsp-ad-progress-list"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang='scss' scoped>
+@mixin position {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.wsp-progress-bar {
+  @include position();
+  z-index: 31;
+  outline: none;
+
+  .wsp-chapters-container {
+    width: 100%;
+    z-index: 32;
+    position: relative;
+    left: 0;
+    height: 100%;
+
+    .wsp-chapter-hover-container {
+      z-index: 32;
+      position: relative;
+      left: 0;
+      height: 100%;
+      float: left;
+
+      .wsp-progress-bar-padding {
+        position: absolute;
+        width: 100%;
+        height: 16px;
+        bottom: 0;
+        z-index: 28;
+      }
+
+      .wsp-progress-list {
+        z-index: 39;
+        background: rgba(255, 255, 255, .2);
+        height: 100%;
+        transform: scaleY(0.6);
+        transition: transform .1s cubic-bezier(0.4, 0, 1, 1), -webkit-transform .1s cubic-bezier(0.4, 0, 1, 1);
+        position: relative;
+
+        .wsp-play-progress {
+          z-index: 34;
+        }
+
+        .wsp-swatch-background-color {
+          background: #fc0;
+          @include position();
+        }
+
+        .wsp-progress-linear-live-buffer {
+          z-index: 45;
+          background: #c00;
+          opacity: 0;
+          transform-origin: 0 0;
+          @include position();
+        }
+
+        .wsp-load-progress {
+          @include position();
+          z-index: 33;
+          background: rgba(255, 255, 255, .4);
+          transform-origin: 0 0;
+          left: 0px;
+          transform: scaleX(0.703556);
+        }
+
+        .wsp-hover-progress {
+          @include position();
+          z-index: 35;
+          background: rgba(0, 0, 0, .125);
+          opacity: 0;
+          transition: opacity .25s cubic-bezier(0, 0, 0.2, 1);
+        }
+
+        .wsp-ad-progress-list {
+          display: none;
+        }
+      }
+    }
+  }
+}
+</style>
--- package/@component/WsPlayer.vue
+++ package/@component/WsPlayer.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-const wsOptions = {}
 const props = withDefaults(defineProps<Props>(), {
   url: '',
 })
@@ -18,12 +17,8 @@ const props = withDefaults(defineProps<Props>(), {
         <div class="wsp-controls">
           <div class="wsp-left-controls">
             <!-- 播放按钮 -->
-            <button class="wsp-button wsp-play-button" title="播放" aria-label="暂停">
-              <svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%">
-                <use class="wsp-svg-shadow" xlink:href="#wsp-id-01" />
-                <path id="ytp-id-84" class="ytp-svg-fill" d="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z" />
-              </svg>
-            </button>
+            <PlayButton />
+            <PlayProgress/>
           </div>
           <div class="wsp-right-controls" />
         </div>
@@ -33,11 +28,13 @@ const props = withDefaults(defineProps<Props>(), {
 </template>
 
 <script lang="ts">
+import PlayButton from "./PlayButton.vue";
+import PlayProgress from './PlayProgress.vue';
+
 interface Props {
   url?: string
 }
 </script>
-
 <style lang="scss" scoped>
-@import './index.scss';
+@import '../@scss/index.scss';
 </style>
